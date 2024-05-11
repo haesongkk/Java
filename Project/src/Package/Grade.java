7cbb8bbc7c3f0a6c57ea71f888cc3170a540be2d
haesongkk <haesoooong@gmail.com>
2024-05-11 18:21:55 +0900

    Java 기본 문법

---
+++ Project/src/Package/Grade.java
@@ -0,0 +1,17 @@
+package Package;
+
+public class Grade {
+	
+	int e;
+	int m;
+	
+	void input_grade(int a, int b) {
+		e=a;
+		m=b;
+	}
+	
+	void output_grade() {
+		System.out.println(e+m);
+	}
+	
+}
--- Project/src/Package/Main.java
+++ Project/src/Package/Main.java
@@ -1,11 +1,33 @@
 package Package;
+import java.util.Scanner;
 
 public class Main {
-
+	
 	public static void main(String[] args) {
 		
-		System.out.println("Hello World");
+		String msg = "Hello, Java!";
+		System.out.println(msg);
+		
+		Grade g = new Grade();
+		g.input_grade(85, 90);
+		g.output_grade();
+	
+		int array[];
+		array = new int[5];
+		
+		for( int i = 0; i < array.length; i++ )
+			array[i] = i;
+		
+		for( int i : array )
+			System.out.print(i + " ");
+		
+		Scanner sc = new Scanner(System.in, "euc-kr");
+		while(sc.hasNextLine()) {
+			System.out.println(sc.nextLine());
+			
+		}
 
+		
 	}
 
 }
